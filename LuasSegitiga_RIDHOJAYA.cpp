eff5d37a677448982b0b45e9bf4ef34e574bb6c2
Ridho <143157206+RIDHOJAYA@users.noreply.github.com>
2023-08-25 12:18:28 +0700

    Add files via upload

---
+++ LuasSegitiga_RIDHOJAYA.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+  int Alas,Tinggi;
+  float Luas;
+  cout<<"Masukan alas :";
+  cin>>Alas;
+  cout<<"Masukan tinggi :";
+  cin>>Tinggi;
+  Luas=0.5*Alas*Tinggi;
+  cout<<"Luas segitiga adalah"<<Luas<<endl;
+  return 0;
+}
