bb4107eb8ec7d13d6ca041237a8fe718e077c2d7
Ridho <143157206+RIDHOJAYA@users.noreply.github.com>
2023-08-25 12:17:52 +0700

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () {
+  string Nama;
+  int Umur;
+  cout <<"Siapa nama kamu ?";
+  cin >> Nama;
+  cout <<"Namaku"<< Nama;
+  cout <<"Berapa umur kamu? /n";
+  cin >> Umur;
+}
